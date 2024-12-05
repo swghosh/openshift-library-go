9f32b5702f1324033bc6c5ee38c055ebff4ffcfd
Swarup Ghosh <swghosh@redhat.com>
2024-12-06 01:47:19 +0530

    fnv64a hash the KMSConfig struct to generate a keyID
    Signed-off-by: Swarup Ghosh <swghosh@redhat.com>

--- pkg/operator/encryption/encryptionconfig/kms_config.go
+++ pkg/operator/encryption/encryptionconfig/kms_config.go
@@ -1,19 +1,27 @@
 package encryptionconfig
 
 import (
+	"encoding/hex"
 	"fmt"
+	"hash"
 	"hash/fnv"
 	"sort"
 	"strings"
 	"time"
 
 	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	configv1 "github.com/openshift/api/config/v1"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 const (
 	KMSPluginEndpoint = "unix:///var/kms-plugin/socket.sock"
 	KMSPluginTimeout  = 5 * time.Second
 )
+
+var hasher = fnv.New64a()
 
 // shortHash returns the 32-bit FNV-1a hash
 func shortHash(s string) string {
@@ -39,4 +47,22 @@ func resourceHash(grs ...schema.GroupResource) string {
 // generateKMSKeyName generates key name for current kms provider
 func generateKMSKeyName(prefix string, gr schema.GroupResource) string {
 	return fmt.Sprintf("%s-%s", prefix, resourceHash(gr))
+}
+
+// HashObject will hash any object using a provided hasher
+func HashObject(hasher hash.Hash, objectToWrite interface{}) string {
+	hasher.Reset()
+	printer := spew.ConfigState{
+		Indent:         " ",
+		SortKeys:       true,
+		DisableMethods: true,
+		SpewKeys:       true,
+	}
+	printer.Fprintf(hasher, "%#v", objectToWrite)
+	return hex.EncodeToString(hasher.Sum(nil)[0:])
+}
+
+// HashKMSConfig returns a short FNV 64-bit hash for a KMSConfig struct
+func HashKMSConfig(config configv1.KMSConfig) string {
+	return HashObject(hasher, config)
 }
